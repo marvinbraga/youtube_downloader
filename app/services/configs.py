c296d3f947d662250cd6ef32a0b0f0c2a76d4b1b
Marcus Vinicus Braga <mvbraga@gmail.com>
2024-11-27 21:04:35 -0300

    uwtv

--- app/services/configs.py
+++ app/services/configs.py
@@ -3,8 +3,9 @@ from typing import Dict, Union
 
 from fastapi.security import HTTPBearer
 
-VIDEO_DIR = Path(__file__).parent.parent.parent / "downloads"
-JSON_CONFIG_PATH = Path(__file__).parent.parent.parent / "data" / "videos.json"
+ROOT_DIR = Path(__file__).parent.parent.parent
+VIDEO_DIR = ROOT_DIR / "downloads"
+JSON_CONFIG_PATH = ROOT_DIR / "data" / "videos.json"
 VIDEO_DIR.mkdir(exist_ok=True)
 video_mapping: Dict[str, Union[Path, str]] = {}
 security = HTTPBearer()
